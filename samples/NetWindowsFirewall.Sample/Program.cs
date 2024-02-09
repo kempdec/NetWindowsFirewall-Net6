94f1062f56dff7de53cb2776e5688bdf27f819a9
Vinícius Lima <viniciusxdl@outlook.com>
2024-02-09 11:43:18 -0300

    Adicionar projeto de amostra.

---
+++ samples/NetWindowsFirewall.Sample/Program.cs
@@ -0,0 +1,24 @@
+﻿using KempDec.NetWindowsFirewall;
+using System.Net;
+
+var ipAddress = IPAddress.Parse("127.0.0.1");
+
+// Exemplo com a instância estática de NetWindowsFirewall:
+// -----------------------------------------------------------------------------------------------------
+
+// Adiciona uma regra de bloqueio (de entrada) para o endereço de IP "127.0.1" no Firewall do Windows.
+NetWindowsFirewall.Instance.AddBlockInIpRule($"IP {ipAddress} bloqueado", ipAddress);
+
+// Adiciona uma regra de bloqueio (de saída) para o endereço de IP "127.0.0.1" no Firewall do Windows.
+NetWindowsFirewall.Instance.AddBlockOutIpRule($"IP {ipAddress} bloqueado", ipAddress);
+
+// Exemplo com criação de uma instância manual:
+// -----------------------------------------------------------------------------------------------------
+
+var netWindowsFirewall = new NetWindowsFirewall();
+
+// Adiciona uma regra de bloqueio (de entrada) para o endereço de IP "127.0.1" no Firewall do Windows.
+netWindowsFirewall.AddBlockInIpRule($"IP {ipAddress} bloqueado", ipAddress);
+
+// Adiciona uma regra de bloqueio (de saída) para o endereço de IP "127.0.0.1" no Firewall do Windows.
+netWindowsFirewall.AddBlockOutIpRule($"IP {ipAddress} bloqueado", ipAddress);
